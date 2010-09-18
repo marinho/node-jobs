5f21c16cd7183a567065eebb99eb266bf70272f4
Marinho Brandão <marinho@gmail.com>
2010-09-17 21:42:52 -0300

    Working on tests for URL calls

--- node.jobs/tests/url-calls.js
+++ node.jobs/tests/url-calls.js
@@ -48,8 +48,9 @@ vows.describe('Node.JobS HTTP URL methods').addBatch({
             var vars = encode_vars({
                 'name': 'testing-on-url-calls',
                 'params': {something: 'some value', other_field: 'other thing'},
-                })
+                });
 
+            // Posts the new job
             var req = cl.request('GET', '/jobs/post/'+vars, {}); req.end();
             req.on('response', function(resp){
                 assert.equal(resp.statusCode, 200);
@@ -90,13 +91,92 @@ vows.describe('Node.JobS HTTP URL methods').addBatch({
                     });
                 });
             },
-        'Get jobs': function(){
-            },
         'Get next job': function(){
-            },
-        'Delete job': function(){
+            var vars = encode_vars({
+                'name': 'testing-next-job',
+                'params': {something: 'some value', other_field: 'other thing'},
+                });
+
+            // Posts a new job
+            var req = cl.request('GET', '/jobs/post/'+vars, {}); req.end();
+            req.on('response', function(resp){
+                assert.equal(resp.statusCode, 200);
+                resp.on('data', function(content){
+                    var job = JSON.parse(content);
+
+                    // Posts other new job
+                    var req2 = cl.request('GET', '/jobs/post/'+vars, {}); req2.end();
+                    req2.on('response', function(resp2){
+                        assert.equal(resp2.statusCode, 200);
+                        resp2.on('data', function(content2){
+                            var job2 = JSON.parse(content2);
+
+                            // Gets the job again to check it has been deleted
+                            var req3 = cl.request('GET', '/jobs/next/?name='+job2['name'], {}); req3.end();
+                            req3.on('response', function(resp3){
+                                assert.equal(resp3.statusCode, 200);
+
+                                resp3.on('data', function(content3){
+                                    var job3 = JSON.parse(content3);
+                                    assert.equal(job['_id'], job3['_id']);
+
+                                    var req4 = cl.request('GET', '/jobs/delete/?name='+job['name'], {}); req4.end();
+                                    req4.on('response', function(resp4){
+                                        assert.equal(resp4.statusCode, 200);
+                                        resp4.on('data', function(deleted){
+                                            assert.equal(deleted, 'true');
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
             },
         'Expire job': function(){
+            var vars = encode_vars({
+                'name': 'testing-expiration',
+                'params': {something: 'some value', other_field: 'other thing'},
+                'expire': new Date()
+                });
+
+            // Posts a new job
+            var req = cl.request('GET', '/jobs/post/'+vars, {}); req.end();
+            req.on('response', function(resp){
+                assert.equal(resp.statusCode, 200);
+                resp.on('data', function(content){
+                    var job = JSON.parse(content);
+
+                    // Expires the job
+                    var req2 = cl.request('GET', '/jobs/expire/', {}); req2.end();
+                    req2.on('response', function(resp2){
+                        assert.equal(resp2.statusCode, 200);
+                        resp2.on('data', function(expired){
+                            assert.equal(expired, 'true');
+
+                            // Gets the job again to check it has been expired
+                            var req3 = cl.request('GET', '/jobs/?name='+job['name'], {}); req3.end();
+                            req3.on('response', function(resp3){
+                                assert.equal(resp3.statusCode, 200);
+
+                                resp3.on('data', function(content3){
+                                    var job3 = JSON.parse(content3)[0];
+                                    assert.equal(job3['status'], 'expired');
+
+                                    var req4 = cl.request('GET', '/jobs/delete/?name='+job['name'], {}); req4.end();
+                                    req4.on('response', function(resp4){
+                                        assert.equal(resp4.statusCode, 200);
+                                        resp4.on('data', function(deleted){
+                                            assert.equal(deleted, 'true');
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
             },
         'Update/Change job': function(){
             },
